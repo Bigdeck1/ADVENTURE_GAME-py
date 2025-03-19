393367973e90347c42bd6ee3385dc7036d361692
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2025-03-19 12:32:39 +0800

    player

---
+++ wag galawin pls/Player.py
@@ -0,0 +1,17 @@
+first_name = input("Enter First name: ")
+Middle_name = input("Enter Middle name:")
+Last_name = input("Enter last name:")
+Register_name = input("Enter your in game name:")
+
+print (f"Your ingame name: {Register_name}")
+def fck():
+    input ("Choose your class:")
+    awit = print("Fighter\n", "Mage\n", "Ranger\n", "Tank\n", "Assasin\n", "Support\n")
+
+def type():
+    print(f"Your Chosen Class:")
+def typefighter():
+    print("Melee","Sword","Broad Sword","Long Sword")
+
+def typemage():
+    print
